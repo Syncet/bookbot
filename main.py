13a92647245df7a2389ef2cc4359941603a2a3dd
Dergall <Galldm@outlook.com>
2025-08-02 10:06:09 -0500

    Importable books

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from stats import get_word_count
 from stats import get_char
 import sys
-print(sys.argv)
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    imported_book = sys.argv[1]
 def get_book_text(book):
     with open(book) as f:
         file_contents = f.read()
@@ -10,7 +14,7 @@ def get_book_text(book):
     
 
 def main():
-    book = ""
+    book = imported_book
     words = get_book_text(book)
     counted_words = get_word_count(words)
     letters = get_char(words)
